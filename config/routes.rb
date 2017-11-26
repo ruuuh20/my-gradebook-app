d97a0b9b5b1158c04448628de9c5c560993bd266
ruuuh20 <pkim2320@gmail.com>
2017-11-26 01:31:09 -0500

    set up omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
