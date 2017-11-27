77f59a2d8b54f6df784d1049af7d5e2a91220fc9
ruuuh20 <pkim2320@gmail.com>
2017-11-27 01:05:20 -0500

    semesters page

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,7 +1,25 @@
 class SemestersController < ApplicationController
 
+  # @SEMESTER_LIST = ['Fall, Spring, Summer']
+
   def index
     @semesters = Semester.all
-    render :text => "Hi"
+
+
+  end
+
+  def new
+    @semester = Semester.new
+  end
+
+  def create
+    @semester = Semester.create(semester_params)
+    if @semester.save
+      flash[:success] = "successfully created"
+      redirect_to semesters_path
+    else
+      flash[:error] = "There was an error"
+      redirect_to root_path
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin1 = User.create(:email => "iamadmin@example.com",
+            :password => "password",
+            :role => :admin)
+student1 = User.create(:email => "iamstudent@example.com",
+            :password => "password",
+            :role => :student)
+teacher1 = User.create(:email => "iamteacher@example.com",
+            :password => "password",
+            :role => :teacher)
+guardian1 = User.create(:email => "iamguardian@example.com",
+            :password => "password",
+            :role => :guardian)
+
+semester1 = Semester.create(:name => "Fall")
+semester2 = Semester.create(:name => "Winter")
+semester3 = Semester.create(:name => "Spring")
+semester4 = Semester.create(:name => "Summer")
