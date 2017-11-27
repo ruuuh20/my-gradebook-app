5b51f56ed01ace6e0840581fe36879478587b0b0
ruuuh20 <pkim2320@gmail.com>
2017-11-27 12:21:29 -0500

    added courses pages

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,9 +4,36 @@ class CoursesController < ApplicationController
   before_action :admin_only, :except => [:index, :show]
 
   def index
-    @semester = Semester.find(:id)
+    @semester = Semester.find(params[:semester_id])
     @courses = @semester.courses.all
     @all_courses = Course.all
+  end
+
+  def new
+    @semester = Semester.find(params[:id])
+    @course = @semester.courses.build(course_params)
+  end
+
+
+  def create
+    @course = Course.new(course_params)
+    @semester = Semester.find(params[:id])
+    @course.semester_id = @semester.id
+    if @course.save
+      flash[:success] = "Successfully created"
+      redirect_to semester_course_path(@semester.id, @course.id)
+    else
+      flash[:error] = "error"
+      redirect_to root_path
+    end
+  end
+
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    flash[:notice] = "Deleted"
+    redirect_to root_path
   end
 
 end
