9a5527242edb068eeeeb583b9f209ede59cfd81b
ruuuh20 <pkim2320@gmail.com>
2017-11-30 02:46:04 -0500

    added assignments forms

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class AssignmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :must_be_admin_or_teacher, :except => [:index, :show]
@@ -27,17 +28,27 @@ class AssignmentsController < ApplicationController
     # @course = Course.find(params[:id])
     @assignment = Assignment.create(assignment_params)
     @assignment.course_id = @course.id
+    if current_user.teacher?
+      @assignment.user_id = current_user.id
+    end
+    # binding.pry
     if @assignment.save
       flash[:notice] = "Assignment was created."
-      redirect_to @course
+      redirect_to  semester_course_assignments_path(@semester, @course, @assignment)
     else
       flash[:error] = "There was an error"
       render 'assignments/new'
     end
   end
 
-
+  def show
+    @assignment = Assignment.find(params[:id])
+    @semester = Semester.find(params[:semester_id])
+    @course = Course.find(params[:course_id])
   end
+
+
+
 
 private
   def assignment_params
